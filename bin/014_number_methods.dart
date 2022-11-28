fb621254198332f276b037d65c1b46dbc18000ab
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2022-11-28 06:01:30 +0600

    First commit

---
+++ bin/014_number_methods.dart
@@ -0,0 +1,24 @@
+void main() {
+  //number properties
+  var x = 4;
+  print('Following are number properties:');
+  print('--------------------------------');
+  print('x.isOdd = ${x.isOdd}');
+  print('x.isEven = ${x.isEven}');
+  print('x.isFinite = ${x.isFinite}');
+  print('x.isInfinite = ${x.isInfinite}');
+  print('x.isNaN = ${x.isNaN}');
+  print('x.isNegative = ${x.isNegative}');
+
+  //number Methods
+  print('');
+  print('Following are number methods:');
+  print('--------------------------------');
+  var y = 8.5;
+  print('y.abs() = ${y.abs()}');
+  print('y.round() = ${y.round()}');
+  print('y.floor() = ${y.floor()}');
+  print('y.truncate() = ${y.truncate()}');
+  print('y.ceil() = ${y.ceil()}');
+  print('y.remainder(x) = ${y.remainder(x)}');
+}
