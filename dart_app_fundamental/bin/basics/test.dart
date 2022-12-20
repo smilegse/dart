f85da7a14b067ddd8c4cc86d22c99ec015f7c300
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2022-12-20 07:06:26 +0600

    commit from home

--- dart_app_fundamental/bin/basics/test.dart
+++ dart_app_fundamental/bin/basics/test.dart
@@ -1,3 +1,18 @@
 void main() {
-  print('this is test dart app');
+  int day = 1;
+  if (day == 1) {
+    print('Sunday');
+  } else if (day == 2) {
+    print('Monday');
+  } else if (day == 3) {
+    print('Tuesday');
+  } else if (day == 4) {
+    print('Wednesday');
+  } else if (day == 5) {
+    print('Thursday');
+  } else if (day == 6) {
+    print('Friday');
+  } else {
+    print('Saturday');
+  }
 }
